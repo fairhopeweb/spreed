146d2fbc1228064a84586fa117272ea9139c34e6
marco <marcoambrosini@pm.me>
2021-09-09 09:32:04 +0200

    Initialise deviceChecker component
    Signed-off-by: marco <marcoambrosini@pm.me>

---
+++ src/components/DeviceChecker/DeviceChecker.vue
@@ -0,0 +1,57 @@
+<!--
+  - @copyright Copyright (c) 2021 Marco Ambrosini <marcoambrosini@pm.me>
+  -
+  - @author Marco Ambrosini <marcoambrosini@pm.me>
+  -
+  - @license GNU AGPL version 3 or any later version
+  -
+  - This program is free software: you can redistribute it and/or modify
+  - it under the terms of the GNU Affero General Public License as
+  - published by the Free Software Foundation, either version 3 of the
+  - License, or (at your option) any later version.
+  -
+  - This program is distributed in the hope that it will be useful,
+  - but WITHOUT ANY WARRANTY; without even the implied warranty of
+  - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  - GNU Affero General Public License for more details.
+  -
+  - You should have received a copy of the GNU Affero General Public License
+  - along with this program. If not, see <http://www.gnu.org/licenses/>.
+-->
+
+<template>
+	<Modal v-if="modal" size="prompt" @close="closeModal">
+		<div class="modal__content">
+			Hello world
+		</div>
+	</Modal>
+</template>
+
+<script>
+import Modal from '@nextcloud/vue/dist/Components/Modal'
+export default {
+	name: 'DeviceChecker',
+
+	components: {
+		Modal,
+	},
+
+	data() {
+		return {
+			modal: false,
+		}
+	},
+	methods: {
+		showModal() {
+			this.modal = true
+		},
+		closeModal() {
+			this.modal = false
+		},
+	},
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
